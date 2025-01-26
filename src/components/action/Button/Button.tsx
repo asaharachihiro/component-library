35dc78dd0b26e6e363130d1eb5c2997513eb747f
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-01-26 16:33:52 +0900

    Button作成

--- src/components/action/Button/Button.tsx
+++ src/components/action/Button/Button.tsx
@@ -3,18 +3,19 @@ import { Slot } from "@radix-ui/react-slot";
 import { Primitive } from "@radix-ui/react-primitive";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../../../utils/cn";
+import { Spinner } from "../../loading/Spinner/Spinner";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center font-bold cursor-pointer ",
+  "inline-flex items-center justify-center font-bold cursor-pointer relative overflow-hidden group",
   {
     variants: {
       variant: {
         primary: "bg-main text-white",
         secondary: "border border-main text-main",
-        textPrimary: "text-main underline underline-offset-4",
-        textSecondary: "text-black-sub underline underline-offset-4",
+        textPrimary: "text-main",
+        textSecondary: "text-black-sub",
         danger: "bg-danger text-white",
-        textDanger: "text-danger underline underline-offset-4",
+        textDanger: "text-danger",
       },
       size: {
         sm: "h-7 px-2 text-sm rounded-md min-w-12",
@@ -22,7 +23,6 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: "primary",
       size: "md",
     },
   }
@@ -32,44 +32,226 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  icon?: string;
+  isActive?: boolean;
+  isLoading?: boolean;
+  variant:
+    | "primary"
+    | "secondary"
+    | "textPrimary"
+    | "textSecondary"
+    | "danger"
+    | "textDanger";
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const ContainedBaseButton = React.forwardRef<
+  HTMLButtonElement,
+  ButtonProps & { variant: "primary" | "danger" }
+>(
   (
-    { className, variant, size, asChild = false, children, disabled, ...props },
+    {
+      asChild = false,
+      children,
+      className,
+      variant,
+      size,
+      disabled,
+      isActive,
+      isLoading = false,
+      icon,
+      ...props
+    },
     ref
   ) => {
-    const Comp = asChild ? Slot : Primitive.button;
-    const computedSize = variant?.startsWith("text") ? "sm" : size;
-    const disabledStyle = cn(
-      disabled && [
-        "text-black-20-opacity border-black-20-opacity pointer-events-none",
-        variant == null || ["primary", "danger"].includes(variant)
-          ? "bg-black-20-opacity"
-          : "",
-      ]
-    );
-    const hoverStyle = disabled
-      ? ""
-      : "bg-black opacity-0 group-hover:opacity-5 transition-all group-active:opacity-10";
+    const Comp = (asChild ? Slot : Primitive.button) as React.ElementType;
 
+    const computedSize = variant === "danger" ? "md" : size;
+    const disabledStyle =
+      disabled || isLoading
+        ? "bg-black-20-opacity text-black-20-opacity border-black-20-opacity pointer-events-none"
+        : "";
     return (
       <Comp
         ref={ref}
         className={cn(
-          "relative overflow-hidden group ",
           buttonVariants({ variant, size: computedSize }),
           disabledStyle,
           className
         )}
         {...props}
       >
-        <div className={cn("absolute inset-0", hoverStyle)} />
-        <span className="flex space-x-1">{children}</span>
+        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 transition-all group-active:opacity-10" />
+        {isLoading && <Spinner className="absolute flex" />}
+        {icon && (
+          <div className={isLoading ? "opacity-0" : "mr-1 flex"}>
+            <span
+              className="material-symbols-rounded"
+              style={{ fontSize: "16px" }}
+            >
+              {icon}
+            </span>
+          </div>
+        )}
+        <span className={cn(isLoading && "opacity-0")}>{children}</span>
       </Comp>
     );
   }
 );
-Button.displayName = "Button";
 
-export { Button, buttonVariants };
+const OutlinedBaseButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      asChild = false,
+      children,
+      className,
+      variant,
+      size,
+      disabled,
+      isActive,
+      isLoading = false,
+      icon,
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = (asChild ? Slot : Primitive.button) as React.ElementType;
+    const disabledStyle =
+      disabled || isLoading
+        ? "text-black-20-opacity border-black-20-opacity pointer-events-none"
+        : "";
+    const activeStyle = isActive && "bg-main-bg";
+
+    return (
+      <Comp
+        ref={ref}
+        className={cn(
+          buttonVariants({ variant, size }),
+          disabledStyle,
+          activeStyle,
+          className
+        )}
+        {...props}
+      >
+        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 transition-all group-active:opacity-10" />
+        {isLoading && <Spinner className="absolute flex" />}
+        {icon && (
+          <div className={isLoading ? "opacity-0" : "mr-1 flex"}>
+            <span
+              className="material-symbols-rounded"
+              style={{ fontSize: "16px" }}
+            >
+              {icon}
+            </span>
+          </div>
+        )}
+        <span className={cn(isLoading && "opacity-0")}>{children}</span>
+      </Comp>
+    );
+  }
+);
+
+const TextBaseButton = React.forwardRef<
+  HTMLButtonElement,
+  ButtonProps & { variant: "textPrimary" | "textSecondary" | "textDanger" }
+>(
+  (
+    {
+      asChild = false,
+      children,
+      className,
+      variant,
+      size,
+      disabled,
+      isActive,
+      isLoading = false,
+      icon,
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = (asChild ? Slot : Primitive.button) as React.ElementType;
+
+    const disabledStyle =
+      disabled || isLoading
+        ? "text-black-20-opacity border-black-20-opacity pointer-events-none"
+        : "";
+    const activeStyle =
+      variant !== "textDanger" && isActive && "bg-main-bg text-main";
+
+    return (
+      <Comp
+        ref={ref}
+        className={cn(
+          buttonVariants({ variant, size }),
+          disabledStyle,
+          activeStyle,
+          className
+        )}
+        {...props}
+      >
+        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-5 transition-all group-active:opacity-10" />
+        {isLoading && <Spinner className="absolute flex" />}
+        {icon && (
+          <div className={isLoading ? "opacity-0" : "mr-1 flex"}>
+            <span
+              className="material-symbols-rounded"
+              style={{ fontSize: "16px" }}
+            >
+              {icon}
+            </span>
+          </div>
+        )}
+        <span
+          className={isLoading ? "opacity-0" : "underline underline-offset-4"}
+        >
+          {children}
+        </span>
+      </Comp>
+    );
+  }
+);
+
+const ButtonComponentMap: Record<ButtonProps["variant"], React.ElementType> = {
+  primary: ContainedBaseButton,
+  secondary: OutlinedBaseButton,
+  textPrimary: TextBaseButton,
+  textSecondary: TextBaseButton,
+  danger: ContainedBaseButton,
+  textDanger: TextBaseButton,
+};
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      isLoading = false,
+      disabled,
+      isActive,
+      icon,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const Component = ButtonComponentMap[variant];
+
+    return (
+      <Component
+        className={className}
+        variant={variant}
+        size={size}
+        asChild={asChild}
+        children={children}
+        disabled={disabled}
+        isActive={isActive}
+        isLoading={isLoading}
+        icon={icon}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
