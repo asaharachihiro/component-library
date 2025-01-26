8212ff1a4641552a8d8211c7d9fac57fa6c7ad48
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-01-26 16:33:28 +0900

    Spinner作成

---
+++ src/components/loading/Spinner/Spinner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { cn } from "../../../utils/cn";
+
+export const Spinner = ({ className }: { className?: string }) => {
+  return (
+    <div className={className}>
+      <span
+        className="material-symbols-rounded  animate-spin text-black-20-opacity"
+        style={{ fontSize: "16px" }}
+      >
+        progress_activity
+      </span>
+    </div>
+  );
+};
