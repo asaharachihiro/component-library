411d8d86359460f49816d57124c1fea4fc516068
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-02-12 13:04:13 +0900

    viteビルダーからwebpackに変更

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,28 +1,47 @@
-const { mergeConfig } = require("vite");
+const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
-
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-links",
     "@storybook/addon-interactions",
+    "@storybook/addon-webpack5-compiler-babel",
     "@chromatic-com/storybook",
   ],
-
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/react-webpack5",
     options: {},
   },
-
   docs: {},
-
   typescript: {
     reactDocgen: "react-docgen-typescript",
   },
+  webpackFinal: async (config) => {
+    config.module.rules = config.module.rules.filter(
+      (rule) =>
+        !(
+          rule.test &&
+          typeof rule.test.test === "function" &&
+          rule.test.test(".css")
+        )
+    );
 
-  viteFinal: async (config) => {
-    config.base = "/";
-    return mergeConfig(config, {});
+    config.module.rules.push({
+      test: /\.css$/,
+      include: path.resolve(__dirname, "../src"),
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: { importLoaders: 1 },
+        },
+        "postcss-loader",
+      ],
+    });
+
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    return config;
   },
 };
--- storybook-static/assets/preview-eOoAemRp.js
+++
@@ -1,1 +0,0 @@
-const o={controls:{expanded:!0}},t=["autodocs","autodocs","autodocs"];export{o as parameters,t as tags};
