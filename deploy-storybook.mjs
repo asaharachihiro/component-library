dc4b714aeea18b0e9fc89f5e376cb80ca4778644
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-02-12 09:07:28 +0900

    パス修正スクリプト追加

---
+++ deploy-storybook.mjs
@@ -0,0 +1,41 @@
+#!/usr/bin/env zx
+
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+import "zx/globals";
+
+const REPO_NAME = "component-library"; //
+
+// ビルド
+await $`npm run build-storybook`;
+
+// ビルドされたものの修正
+await within(async () => {
+  cd("storybook-static");
+
+  // .nojekyllファイルを作成
+  await $`touch .nojekyll`;
+
+  // iframe.htmlがassets/iframe.*.jsを読み込めるように修正
+  const iframehtml = await fs.readFile("iframe.html", "utf-8");
+  await fs.writeFile(
+    "iframe.html",
+    iframehtml.replace(/\/assets/g, `/${REPO_NAME}/assets`)
+  );
+
+  // assets内の*.jsや*.map.jsファイルにも同様のパス修正が必要
+  const assetjs_path = await glob("assets/*.@(map|js)");
+  await assetjs_path.forEach(async (path) => {
+    const assetjs = await fs.readFile("./" + path, "utf-8");
+    await fs.writeFile(
+      "./" + path,
+      assetjs.replace(/assets\//g, `${REPO_NAME}/assets/`)
+    );
+  });
+});
+
+// キャッシュ削除
+await $`rm -rf node_modules/.cache/gh-pages`;
+
+// デプロイ
+await $`gh-pages -d storybook-static`;
