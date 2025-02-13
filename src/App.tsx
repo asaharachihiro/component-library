9e71aeba896824191ed04e93b86632ac72b87cf8
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-02-13 10:27:06 +0900

    app試用記述削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 import * as React from "react";
 import "./styles/global.css";
-import { Link } from "./components/action/Link";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <Link
-        target="_blank"
-        children={"外部サイト(Tailwind - font-size)"}
-        href={"https://tailwindcss.com/docs/font-size#examples"}
-        className="block"
-      />
-      <Link
-        target="_blank"
-        children={"外部サイト(Tailwind - font-size)"}
-        href={"https://tailwindcss.com/docs/font-size#examples"}
-        className="block"
-      />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
