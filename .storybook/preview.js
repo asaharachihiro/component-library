2dc99f95962a6709fad89d4ace69039b2b93c6a5
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-02-18 13:23:40 +0900

    Storybookの表示順設定を追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,27 @@
 import "../src/styles/global.css";
 export const parameters = {
   controls: { expanded: true },
+  options: {
+    storySort: {
+      order: [
+        "Action",
+        ["Button", "IconButton", "Link"],
+        "Input",
+        ["TextBox", "SeachBox", "DatePicker", "FilePicker", "Slider", "Form"],
+        "Selection",
+        ["SelectBox", "CheckBox", "ToggleSwitch", "RadioButton"],
+        "List",
+        ["List", "Accordion", "Card", "Table", "FilterButton"],
+        "Notification",
+        ["Badge", "Tag", "Dialog", "Toast", "Tooltip", "MeatballMenu"],
+        "Navigation",
+        ["MenuBar", "SideMenu", "Breadcrumbs", "Stepper"],
+        "Display",
+        ["Tabs", "Mordal", "Pagenation"],
+        "Loading",
+        ["Spinner", "ProgressBar", "Skeleton"],
+      ],
+    },
+  },
 };
-export const tags = ["autodocs", "autodocs", "autodocs", "autodocs"];
+export const tags = ["autodocs"];
