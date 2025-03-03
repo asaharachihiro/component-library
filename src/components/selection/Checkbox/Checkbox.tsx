383edcbbb05da00ed70f338d410250007151541d
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-03-03 13:16:31 +0900

    Checkbox作成

---
+++ src/components/selection/Checkbox/Checkbox.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { cn } from "../../../utils/cn";
+
+interface CheckboxProps {
+  id: string;
+  className?: string;
+  checked?: boolean | "indeterminate";
+  disabled?: boolean;
+  isValid?: boolean;
+  children?: React.ReactNode;
+  onCheckedChange?: (checked: boolean | "indeterminate") => void;
+}
+
+export const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  CheckboxProps
+>(
+  (
+    {
+      id,
+      className,
+      children,
+      checked = false,
+      onCheckedChange,
+      disabled = false,
+      isValid = true,
+      ...props
+    },
+    ref
+  ) => {
+    const [internalChecked, setInternalChecked] = React.useState<
+      boolean | "indeterminate"
+    >(checked);
+
+    React.useEffect(() => {
+      if (checked !== undefined) {
+        setInternalChecked(checked);
+      }
+    }, [checked]);
+
+    const handleCheckedChange = () => {
+      let newChecked: boolean | "indeterminate";
+      if (internalChecked === "indeterminate" || !internalChecked) {
+        newChecked = true;
+      } else {
+        newChecked = false;
+      }
+
+      setInternalChecked(newChecked);
+      if (onCheckedChange) {
+        onCheckedChange(newChecked);
+      }
+    };
+
+    const disabledStyle =
+      disabled && "text-black-20-opacity pointer-events-none";
+    const errorStyle = !disabled && !isValid && "text-danger";
+    const isNomalStyle = !disabled && isValid;
+
+    return (
+      <CheckboxPrimitive.Root
+        id={id}
+        className={cn(
+          "inline-flex h-7 w-7 select-none items-center justify-center rounded-md text-2xl hover:bg-black-5-opacity focus:outline-none focus-visible:ring-2 focus-visible:ring-black-sub active:bg-black-10-opacity",
+          disabledStyle,
+          errorStyle,
+          className
+        )}
+        checked={internalChecked === "indeterminate" ? false : internalChecked}
+        onCheckedChange={handleCheckedChange}
+        {...props}
+        ref={ref}
+        disabled={disabled}
+      >
+        <span className="material-symbols-rounded">
+          {internalChecked === "indeterminate" ? (
+            <span className={cn("icon-fill", isNomalStyle && "text-main")}>
+              indeterminate_check_box
+            </span>
+          ) : internalChecked ? (
+            <span className={cn("icon-fill", isNomalStyle && "text-main")}>
+              check_box
+            </span>
+          ) : (
+            <span className={cn(isNomalStyle && "text-black-sub")}>
+              check_box_outline_blank
+            </span>
+          )}
+        </span>
+      </CheckboxPrimitive.Root>
+    );
+  }
+);
+Checkbox.displayName = "Checkbox";
