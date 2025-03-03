aaae04c8993e2a5c0bb9f1ec523b9a2058427c94
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-03-03 13:37:15 +0900

    stories作成

---
+++ src/components/selection/Checkbox/Chekbox.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from ".";
+import * as React from "react";
+
+const meta: Meta<typeof Checkbox> = {
+  title: "Components/Selection/Checkbox",
+  component: Checkbox,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: "Checkboxは、複数選択のために使用されるコンポーネントです。",
+      },
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Checkbox>;
+
+const Template: Story = {
+  render: (args) => {
+    const [checked, setChecked] = React.useState(args.checked);
+
+    React.useEffect(() => {
+      setChecked(args.checked);
+    }, [args.checked]);
+
+    const handleCheckedChange = (newChecked: boolean | "indeterminate") => {
+      setChecked(newChecked);
+    };
+
+    return (
+      <Checkbox
+        {...args}
+        checked={checked}
+        onCheckedChange={handleCheckedChange}
+      />
+    );
+  },
+};
+
+export const Default: Story = {
+  ...Template,
+  args: {
+    id: "1",
+    checked: false,
+  },
+  argTypes: {
+    checked: {
+      control: { type: "radio" },
+      options: [true, false, "indeterminate"],
+    },
+  },
+};
