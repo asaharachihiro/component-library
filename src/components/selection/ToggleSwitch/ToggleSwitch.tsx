b24b2b41820f56c9a246ecd5073d4009285ff7fe
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-03-11 10:11:06 +0900

    ToggleSwitch作成

---
+++ src/components/selection/ToggleSwitch/ToggleSwitch.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { cn } from "../../../utils/cn";
+
+interface ToggleSwitchProps {
+  id: string;
+  className?: string;
+  label?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  isValid?: boolean;
+  children?: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const ToggleSwitch = React.forwardRef<
+  HTMLInputElement,
+  ToggleSwitchProps
+>(
+  (
+    {
+      id,
+      className,
+      label,
+      children,
+      checked = false,
+      onClick,
+      disabled = false,
+      isValid = true,
+      ...props
+    },
+    ref
+  ) => {
+    const [toggle, setToggle] = React.useState<boolean>(checked);
+    React.useEffect(() => {
+      setToggle(checked);
+    }, [checked]);
+
+    const handleToggled = () => {
+      let newChecked = !toggle;
+      setToggle(newChecked);
+    };
+    const switchStyle = cn(
+      "flex shrink-0 items-center mt-1 h-7 w-[52px] rounded-full border-2 relative after:absolute after:content-[''] after:w-[16px] after:h-[16px] after:rounded-full  after:transition-transform after:duration-300",
+      {
+        "border-none after:bg-white after:translate-x-[28px]": toggle,
+        "bg-main": toggle && !disabled,
+        "bg-black": toggle && disabled,
+        "after:translate-x-[4px] border-black after:bg-black": !toggle,
+        "opacity-50": !toggle && !disabled,
+        "opacity-20": disabled,
+      }
+    );
+
+    return (
+      <label
+        htmlFor={id}
+        className={cn(
+          disabled
+            ? "pointer-events-none text-black-20-opacity"
+            : "cursor-pointer",
+          "flex justify-between"
+        )}
+      >
+        {label && (
+          <span className="m-1 mr-4 text-left text-base leading-[180%]">
+            {label}
+          </span>
+        )}
+        <input
+          id={id}
+          ref={ref}
+          type="checkbox"
+          checked={toggle}
+          onChange={handleToggled}
+          disabled={disabled}
+          className="hidden"
+          {...props}
+        />
+        <div className={switchStyle} />
+      </label>
+    );
+  }
+);
+ToggleSwitch.displayName = "ToggleSwitch";
