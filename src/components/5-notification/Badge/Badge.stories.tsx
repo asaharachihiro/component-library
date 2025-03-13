01bd25be0b8ecd9efd7df37ffa5505aa6f5be909
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-03-13 18:38:23 +0900

    Badge作成

---
+++ src/components/5-notification/Badge/Badge.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Badge } from ".";
+import { IconButton } from "../../1-action/IconButton";
+
+const meta: Meta<typeof Badge> = {
+  title: "Components/Notification/Badge",
+  component: Badge,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "Badgeは、未読の通知や未完了のタスクを知らせるコンポーネントです。",
+      },
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Badge>;
+
+const Template: Story = {
+  render: (args) => {
+    return (
+      <div className="relative inline-block">
+        <Badge {...args} />
+        <IconButton icon="notifications" id="iconButton" size="m" />
+      </div>
+    );
+  },
+};
+
+export const Default: Story = {
+  ...Template,
+  args: {
+    id: "1",
+    count: 25,
+    type: "counter",
+  },
+  argTypes: {
+    count: { control: { type: "number" } },
+    type: { control: { radio: ["counter", "dot"] } },
+  },
+};
---
+++ src/components/5-notification/Badge/Badge.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { cn } from "../../../utils/cn";
+
+interface BadgeProps {
+  id: string;
+  count: number;
+  className?: string;
+  type?: "counter" | "dot";
+}
+
+export const Badge: React.FC<BadgeProps> = ({
+  id,
+  count = 0,
+  className = "",
+  type = "dot",
+}) => {
+  const displayCount = count && count > 99 ? "99+" : count;
+  return (
+    <>
+      {count > 0 && (
+        <div
+          id={id}
+          className={cn(
+            className,
+            "pointer-events-none absolute z-10 flex shrink-0 select-none rounded-full bg-danger",
+            type === "dot"
+              ? "right-1 top-1 h-1 w-1"
+              : "-right-[2px] -top-[2px] h-4 min-w-4 items-center justify-center px-1 text-[10px] font-bold text-white"
+          )}
+        >
+          {type === "counter" && displayCount}
+        </div>
+      )}
+    </>
+  );
+};
+
+Badge.displayName = "Badge";
