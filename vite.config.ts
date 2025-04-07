17e130e0983cf54bcca5a9545b4253bebed9cd97
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-04-07 13:51:33 +0900

    Update to version 1.1.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,60 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "storybook-static",
-    sourcemap: true,
-    chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@storybook")) {
-              return "storybook";
-            }
-            return "vendor";
-          }
+export default defineConfig(({ mode }) => {
+  if (mode === "storybook") {
+    // Storybook用のビルド設定
+    return {
+      plugins: [react()],
+      base: "/",
+      build: {
+        outDir: "storybook-static",
+        sourcemap: true,
+        chunkSizeWarningLimit: 2000,
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes("node_modules")) {
+                if (id.includes("@storybook")) {
+                  return "storybook";
+                }
+                return "vendor";
+              }
+            },
+          },
         },
       },
+      server: {
+        fs: {
+          allow: ["."],
+        },
+        hmr: {
+          overlay: false,
+        },
+      },
+    };
+  }
+
+  return {
+    plugins: [react()],
+    build: {
+      lib: {
+        entry: "./src/index.ts",
+        name: "ComponentLibrary",
+        formats: ["es", "cjs"],
+        fileName: (format) => `index.${format}.js`,
+      },
+      rollupOptions: {
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+      },
+      outDir: "dist", // ライブラリの出力先
+      sourcemap: true, // ソースマップを生成
     },
-  },
-  server: {
-    fs: {
-      allow: ["."],
-    },
-    hmr: {
-      overlay: false,
-    },
-  },
+  };
 });
