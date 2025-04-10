e36204a77b49f06ef461954afedbda2b2f9f9628
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-04-10 13:34:51 +0900

    パス修正

--- src/components/3-selection/SelectBox/SelectBox.tsx
+++ src/components/3-selection/SelectBox/SelectBox.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useFormContext } from "../../2-input/Form";
 import { useClickOutside } from "../../../utils/useClickOutside";
 import { BaseSelectBox } from "./BaseSelectBox";
-import { List } from "../../4-list/List";
+import { List } from "../../4-list/List/List";
 
 interface SelectBoxProps {
   id: string;
