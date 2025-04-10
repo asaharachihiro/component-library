166a1e07098132e06427043a6b4b1068691c2699
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-04-10 13:06:43 +0900

    main.jsを修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  stories: ["../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-links",
