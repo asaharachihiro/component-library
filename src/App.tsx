c545131b40924cc06e862002d2d28851cb36c109
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-05-22 13:34:41 +0900

    appのサンプル実装削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import * as React from "react";
 import "./styles/global.scss";
-import { Skelton } from "@components/8-loading/Skelton";
 const App: React.FC = () => {
-  return (
-    <div className="w-[500px] space-y-4 p-6">
-      <Skelton className="h-4 w-[200px]" />
-      <Skelton className="h-4 w-[200px]" />
-      <Skelton className="h-4 w-[200px]" />
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
