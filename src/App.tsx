9d02f3f3469e4be5179eb96cea3632d865308d75
asahara.chihiro <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2025-06-12 14:48:08 +0900

    AppにsampleTableを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import * as React from "react";
 import "./styles/global.scss";
+import { Table } from "./components";
+import {
+  sampleAnimalsData,
+  columns,
+} from "./components/4-list/Table/sampleData";
+import { ColumnDef } from "node_modules/@tanstack/table-core/build/lib/types";
+
+type SampleData = {
+  id: string;
+  name: string;
+  length?: string;
+  distribution?: string[];
+  rarity: string;
+  species: string;
+  summary?: string;
+};
+
 const App: React.FC = () => {
-  return <></>;
+  return (
+    <div className="h-[600px] w-[1000px] p-6">
+      <Table
+        data={sampleAnimalsData}
+        columns={columns as ColumnDef<SampleData>[]}
+        columnWidth={125}
+        columnPinning={{
+          left: ["id", "name"],
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
